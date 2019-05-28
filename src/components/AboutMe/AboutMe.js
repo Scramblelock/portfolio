1eb799587199b439a2d6620d6adbc7dc79e107ff
Marc Sakalauskas <scrambledlegs39@hotmail.com>
2019-05-28 19:10:18 -0400

    initial commit

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+
+	return (
+		<div className='aboutMe'>
+			About Me
+		</div>
+	);
+};
+
+export default AboutMe;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navigation = () => {
+
+	return (
+		<div className='container'>
+			<div className='smallLogoComponent'>
+				<img className='marcLogo' alt='marcLogo'/> 
+			</div>
+			<div className='about'>
+				About
+			</div>
+			<div className='contact'>	
+				Contact
+			</div>
+		</div>
+	);
+};
+
+export default Navigation;
