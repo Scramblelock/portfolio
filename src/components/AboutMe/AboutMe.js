8a85104040a2048bc1f1c1e2a6405309bab95166
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-05-31 16:52:58 -0400

    text adjustments

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -15,7 +15,6 @@ const AboutMe = () => {
 				Ut sed amet sed commodo incididunt magna reprehenderit aute in mollit. Dolore laboris reprehenderit ut qui magna tempor exercitation sed cillum mollit id amet aliqua tempor eiusmod mollit aliquip. Voluptate sit aliqua aute dolore reprehenderit ad consequat esse officia veniam dolore ut dolor dolore. Anim ad ex ut magna duis id officia sit duis nulla nostrud consequat cillum eu.Consequat sunt in aute tempor reprehenderit adipisicing incididunt reprehenderit nulla amet pariatur pariatur ut ut occaecat in tempor minim. Veniam dolor ad occaecat dolore sint in ut duis consequat nisi.
 			</p>
 		</div>
-
 	);
 };
 
