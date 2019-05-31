e3b2da453cd0ad8f4aff72436d19ece8e3776eba
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-05-31 16:39:38 -0400

    initial AboutMe section build

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './AboutMe.css';
 
 const AboutMe = () => {
 
 	return (
 		<div className='aboutMe'>
-			About Me
+			<div className='header'>
+				About Me
+			</div>
+			<p className='aboutText'>
+				Voluptate sit aliqua aute dolore reprehenderit ad consequat esse officia veniam dolore ut dolor dolore. Anim ad ex ut magna duis id officia sit duis nulla nostrud consequat cillum eu.Consequat sunt in aute tempor reprehenderit adipisicing incididunt reprehenderit nulla amet pariatur pariatur ut ut occaecat in tempor minim. Veniam dolor ad occaecat dolore sint in ut duis consequat nisi.
+			</p>
+			<p className='aboutText'>
+				Ut sed amet sed commodo incididunt magna reprehenderit aute in mollit. Dolore laboris reprehenderit ut qui magna tempor exercitation sed cillum mollit id amet aliqua tempor eiusmod mollit aliquip. Voluptate sit aliqua aute dolore reprehenderit ad consequat esse officia veniam dolore ut dolor dolore. Anim ad ex ut magna duis id officia sit duis nulla nostrud consequat cillum eu.Consequat sunt in aute tempor reprehenderit adipisicing incididunt reprehenderit nulla amet pariatur pariatur ut ut occaecat in tempor minim. Veniam dolor ad occaecat dolore sint in ut duis consequat nisi.
+			</p>
 		</div>
+
 	);
 };
 
