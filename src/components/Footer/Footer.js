2865450c06e003fb9c57612b3ee283785e030b79
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-06-03 10:23:43 -0400

    styling of social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,13 +12,14 @@ const Footer = () => {
 	return (
 		<div className='footer'>
 			<ul className='socialIcons'>
-				<li><img src={linkedin} alt=''/></li>
-				<li><img src={github} alt=''/></li>
-				<li><img src={facebook} alt=''/></li>
-				<li><img src={instagram} alt=''/></li>
-				<li><img src={twitter} alt=''/></li>
-				<li><img src={gmail} alt=''/></li>
+				<li><a href='https://www.linkedin.com/in/scramblelock/' target="_blank" rel="noopener noreferrer"><img src={linkedin} alt=''/></a></li>
+				<li><a href='https://github.com/Scramblelock' target="_blank" rel="noopener noreferrer"><img src={github} alt=''/></a></li>
+				<li><a href='https://www.facebook.com/scramblelock' target="_blank" rel="noopener noreferrer"><img src={facebook} alt=''/></a></li>
+				<li><a href='https://www.instagram.com/scramblelock/' target="_blank" rel="noopener noreferrer"><img src={instagram} alt=''/></a></li>
+				<li><a href='https://twitter.com/Scramblelock' target="_blank" rel="noopener noreferrer"><img src={twitter} alt=''/></a></li>
+				<li><a href='mailto:scramblelock@gmail.com'><img src={gmail} alt=''/></a></li>
 			</ul>
+			<p>MARC SAKALAUSKAS © 2019</p>
 		</div>
 	);
 };
