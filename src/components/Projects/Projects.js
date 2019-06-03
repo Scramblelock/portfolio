7430af45e41ee1885b7a351c5a5ec095c82e24f8
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-06-03 12:44:46 -0400

    set up project grid

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Projects.css';
+import krang from './Images/krangfull.png';
+import robot from './Images/robofriends.png';
+import hackthenews from './Images/hackthenews.png';
+import toronto from './Images/toronto.png';
+import huddle from './Images/huddle.png';
+import locking4life from './Images/locking4life.png';
 
 const Projects = () => {
 
@@ -8,7 +14,13 @@ const Projects = () => {
 			<div className='header'>
 				Projects
 			</div>
-			<div className='projectImages'>
+			<div className='projectGrid'>
+				<div className='projectBox'><img src={krang} alt=''/></div>
+				<div className='projectBox'><img src={robot} alt=''/></div>
+				<div className='projectBox'><img src={hackthenews} alt=''/></div>
+				<div className='projectBox'><img src={toronto} alt=''/></div>
+				<div className='projectBox'><img src={huddle} alt=''/></div>
+				<div className='projectBox'><img src={locking4life} alt=''/></div>
 			</div>
 		</div>
 	);
