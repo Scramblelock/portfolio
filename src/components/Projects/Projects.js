561b996a42478c14be2bc942ad3c07e50ad40847
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-06-03 17:14:25 -0400

    updated project description

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -24,7 +24,7 @@ const Projects = () => {
 						<img src={krang} alt=''/>
 						<div className='overlay'>
 							Smart Brain
-							<p>React + Node.js</p>
+							<p>React + Node.js + PostgreSQL</p>
 						</div>
 					</a>
 				</div>
@@ -50,7 +50,7 @@ const Projects = () => {
 						<img src={hackthenews} alt=''/>
 						<div className='overlay'>
 							Hack The News
-							<p>React</p>
+							<p>React + Axios</p>
 						</div>
 					</a>
 				</div>
