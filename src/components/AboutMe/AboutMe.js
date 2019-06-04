4e72ef9d384c4e4d91556c198200b13ca7d60fe0
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-06-04 17:41:16 -0400

    first draft of about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,10 +9,13 @@ const AboutMe = () => {
 				About Me
 			</div>
 			<p className='aboutText'>
-				Voluptate sit aliqua aute dolore reprehenderit ad consequat esse officia veniam dolore ut dolor dolore. Anim ad ex ut magna duis id officia sit duis nulla nostrud consequat cillum eu.Consequat sunt in aute tempor reprehenderit adipisicing incididunt reprehenderit nulla amet pariatur pariatur ut ut occaecat in tempor minim. Veniam dolor ad occaecat dolore sint in ut duis consequat nisi.
+				Based in Montréal, Québec, Canada, I'm a highly motivated, self-taught full-stack web developer with a diverse background in arts and sciences and a passion for learning new technologies. I have always pursued meaningful opportunities in my career and am driven by a desire to make a difference in people's lives. 
 			</p>
 			<p className='aboutText'>
-				Ut sed amet sed commodo incididunt magna reprehenderit aute in mollit. Dolore laboris reprehenderit ut qui magna tempor exercitation sed cillum mollit id amet aliqua tempor eiusmod mollit aliquip. Voluptate sit aliqua aute dolore reprehenderit ad consequat esse officia veniam dolore ut dolor dolore. Anim ad ex ut magna duis id officia sit duis nulla nostrud consequat cillum eu.Consequat sunt in aute tempor reprehenderit adipisicing incididunt reprehenderit nulla amet pariatur pariatur ut ut occaecat in tempor minim. Veniam dolor ad occaecat dolore sint in ut duis consequat nisi.
+				As a developer I enjoy making web pages fast and easy to use, while applying the latest technologies. My background as a scientist has helped give me a sharp and disciplined attention to detail. I also have a very strong artistic side, the result of being involved in Hip Hop culture (as a dancer and artist) for over 20 years. My graphic design, video editing and brush lettering skills developed from a love for graffiti and street art. The approach I took to learning dances like b-boying and locking has helped me become a creative artist, community leader and problem solver while also developing effective teaching methods.
+			</p>
+			<p className='aboutText'>
+				I've travelled the world, working with people from all walks of life. Following the motto of "each one, teach one", I truly believe we all have something special to bring to the table. <a className='touchLink' href="#contact"><b>Get in touch</b></a> to see what I can bring to your team!
 			</p>
 		</div>
 	);
