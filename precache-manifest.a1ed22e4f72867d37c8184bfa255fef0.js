5bba0e88a514bba72b9f9d19e79022575eb98d32
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-06-07 16:46:44 -0400

    Updates

---
+++ precache-manifest.a1ed22e4f72867d37c8184bfa255fef0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f1a7a8b529a330196dc94945282e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88b532cd5a9d10d6da78",
+    "url": "/static/css/2.d4c5f937.chunk.css"
+  },
+  {
+    "revision": "697fd3aab4ec128084fd",
+    "url": "/static/css/main.04de7413.chunk.css"
+  },
+  {
+    "revision": "88b532cd5a9d10d6da78",
+    "url": "/static/js/2.1d99b7cc.chunk.js"
+  },
+  {
+    "revision": "697fd3aab4ec128084fd",
+    "url": "/static/js/main.6aa2a610.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a2b56f651762d11508d4db5482ed0387",
+    "url": "/static/media/canadianTuxedo.a2b56f65.jpg"
+  },
+  {
+    "revision": "7601475d429d35f32dd9839ea1c7186a",
+    "url": "/static/media/hackthenews.7601475d.png"
+  },
+  {
+    "revision": "1d4e23da3a25322645f286dba85eb702",
+    "url": "/static/media/huddle.1d4e23da.png"
+  },
+  {
+    "revision": "2199183e31d5094a33065a8a6405a234",
+    "url": "/static/media/krangfull.2199183e.png"
+  },
+  {
+    "revision": "14a1e96e65127890d639e05d76a50361",
+    "url": "/static/media/locking4life.14a1e96e.png"
+  },
+  {
+    "revision": "5c3ca6e386b04b05974c70aa01c591a1",
+    "url": "/static/media/marcLogo.5c3ca6e3.png"
+  },
+  {
+    "revision": "07877978f966f0a02e42bfc2de847123",
+    "url": "/static/media/robofriends.07877978.png"
+  },
+  {
+    "revision": "691d4f1666bea986438ca28042989698",
+    "url": "/static/media/toronto.691d4f16.png"
+  }
+]);
