3fbe16bb9251afdec1d65c5ce1949d34b8ab124a
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-06-26 16:00:07 -0400

    update image slider image links

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Slider from 'react-animated-slider';
+import MarcBrush from './Logos/MarcBrush.png';
+import MarcCode from './Logos/MarcCode.png';
+import MarcGraf1 from './Logos/MarcGraf1.png';
 import 'react-animated-slider/build/horizontal.css';
 import './Landing.css';
 
+
 const content = [
 	{
-		image: 'https://cdn.filestackcontent.com/ANI9fS5qSayY5tX9tYEmZz/resize=width:1200,height:520,fit:scale/output=secure:true/security=policy:eyJleHBpcnkiOiAxNTU5MzMyMjA4LCAiY2FsbCI6IFsicmVhZCIsICJzdGF0IiwgImNvbnZlcnQiLCAicGljayIsICJzdG9yZSJdfQ==,signature:dbfb839bb37b2cb8dacc4143ff59b7054ac409b0c65af9c2d980f04203bdcda4/txQLhr73TKapsp3kGaeY'
+		image: MarcBrush
 	},
 	{
-		image: 'https://cdn.filestackcontent.com/ANI9fS5qSayY5tX9tYEmZz/resize=width:1200,height:520,fit:scale/output=secure:true/security=policy:eyJleHBpcnkiOiAxNTU5MzMyMjA4LCAiY2FsbCI6IFsicmVhZCIsICJzdGF0IiwgImNvbnZlcnQiLCAicGljayIsICJzdG9yZSJdfQ==,signature:dbfb839bb37b2cb8dacc4143ff59b7054ac409b0c65af9c2d980f04203bdcda4/zk4k699zTrKajLsTNxSG'
+		image: MarcCode
 	},
 	{
-		image: 'https://cdn.filestackcontent.com/ANI9fS5qSayY5tX9tYEmZz/resize=width:1200,height:520,fit:scale/output=secure:true/security=policy:eyJleHBpcnkiOiAxNTU5MzMyMjA4LCAiY2FsbCI6IFsicmVhZCIsICJzdGF0IiwgImNvbnZlcnQiLCAicGljayIsICJzdG9yZSJdfQ==,signature:dbfb839bb37b2cb8dacc4143ff59b7054ac409b0c65af9c2d980f04203bdcda4/sl1Nha93RbmCcvu0Tpak'
+		image: MarcGraf1
 	} 
 ]
 
