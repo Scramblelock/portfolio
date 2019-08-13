72a7231ef73d87c31840091e06d9688eea16ecc2
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-08-13 11:54:31 -0400

    add 2 new coding projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,107 +1,134 @@
-import React from 'react';
-import './Projects.css';
-import krang from './Images/krangfull.png';
-import robot from './Images/robofriends.png';
-import hackthenews from './Images/hackthenews.png';
-import toronto from './Images/toronto.png';
-import huddle from './Images/huddle.png';
-import locking4life from './Images/locking4life.png';
+import React from "react";
+import "./Projects.css";
+import krang from "./Images/krangfull.png";
+import robot from "./Images/robofriends.png";
+import hackthenews from "./Images/hackthenews.png";
+import toronto from "./Images/toronto.png";
+import huddle from "./Images/huddle.png";
+import locking4life from "./Images/locking4life.png";
+import nekofudo from "./Images/nekofudo.png";
+import osheaga from "./Images/osheaga.png";
 
 const Projects = () => {
-
-	return (
-		<div className='projects' id='projects'>
-			<div className='header'>
-				Projects
-			</div>
-			<div className='projectGrid'>
-				<div className='projectBox'>
-					<a 
-						href='https://smart-brain-marc.herokuapp.com/' 
-						target='_blank' 
-						rel='noopener noreferrer'
-					>
-						<div className='contentOverlay'></div>
-						<img className='contentImage' src={krang} alt=''/>
-						<div className='contentDetails fadeInBottom'>
-							<h3>Smart Brain</h3>
-							<p>React + Node.js + PostgreSQL</p>
-						</div>
-					</a>
-				</div>
-				<div className='projectBox'>
-					<a 
-						href='https://scramblelock.github.io/robofriends-redux-pwa/' 
-						target='_blank' 
-						rel='noopener noreferrer'
-					>
-						<div className='contentOverlay'></div>
-						<img className='contentImage' src={robot} alt=''/>
-						<div className='contentDetails fadeInBottom'>
-							<h3>Robofriends</h3>
-							<p>React + Redux</p>
-						</div>
-					</a>
-				</div>
-				<div className='projectBox'>
-					<a 
-						href='https://scramblelock.github.io/hack-the-news/' 
-						target='_blank' 
-						rel='noopener noreferrer'
-					>
-						<div className='contentOverlay'></div>
-						<img className='contentImage' src={hackthenews} alt=''/>
-						<div className='contentDetails fadeInBottom'>
-							<h3>Hack The News</h3>
-							<p>React + Axios</p>
-						</div>
-					</a>
-				</div>
-				<div className='projectBox'>
-					<a 
-						href='https://scramblelock.github.io/toronto-waste-lookup/' 
-						target='_blank' 
-						rel='noopener noreferrer'
-					>
-						<div className='contentOverlay'></div>
-						<img className='contentImage' src={toronto} alt=''/>
-						<div className='contentDetails fadeInBottom'>
-							<h3>Toronto Waste Lookup</h3>
-							<p>React</p>
-						</div>
-					</a>
-				</div>
-				<div className='projectBox'>
-					<a 
-						href='https://scramblelock.github.io/coding_challenge_11/' 
-						target='_blank' 
-						rel='noopener noreferrer'
-					>
-						<div className='contentOverlay'></div>
-						<img className='contentImage' src={huddle} alt=''/>
-						<div className='contentDetails fadeInBottom'>
-							<h3>Huddle</h3>
-							<p>HTML + CSS + Bootstrap</p>
-						</div>
-					</a>
-				</div>
-				<div className='projectBox'>
-					<a 
-						href='https://scramblelock.com' 
-						target='_blank' 
-						rel='noopener noreferrer'
-					>
-						<div className='contentOverlay'></div>
-						<img className='contentImage' src={locking4life} alt=''/>
-						<div className='contentDetails fadeInBottom'>
-							<h3>Scramblelock</h3>
-							<p>Wordpress + HTML + CSS</p>
-						</div>
-					</a>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='projects' id='projects'>
+      <div className='header'>Projects</div>
+      <div className='projectGrid'>
+        <div className='projectBox'>
+          <a
+            href='https://smart-brain-marc.herokuapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={krang} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Smart Brain</h3>
+              <p>React + Node.js + PostgreSQL</p>
+            </div>
+          </a>
+        </div>
+        <div className='projectBox'>
+          <a
+            href='https://scramblelock.github.io/robofriends-redux-pwa/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={robot} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Robofriends</h3>
+              <p>React + Redux</p>
+            </div>
+          </a>
+        </div>
+        <div className='projectBox'>
+          <a
+            href='https://scramblelock.github.io/hack-the-news/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={hackthenews} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Hack The News</h3>
+              <p>React + Axios</p>
+            </div>
+          </a>
+        </div>
+        <div className='projectBox'>
+          <a
+            href='https://chingu-voyages.github.io/v10-geckos-team-02/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={nekofudo} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Neko Fudo</h3>
+              <p>React + CSS (Group Project)</p>
+            </div>
+          </a>
+        </div>
+        <div className='projectBox'>
+          <a
+            href='https://fierce-escarpment-13373.herokuapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={osheaga} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Osheaga Trip Planner</h3>
+              <p>React + Axios + i18next</p>
+            </div>
+          </a>
+        </div>
+        <div className='projectBox'>
+          <a
+            href='https://scramblelock.github.io/toronto-waste-lookup/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={toronto} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Toronto Waste Lookup</h3>
+              <p>React</p>
+            </div>
+          </a>
+        </div>
+        <div className='projectBox'>
+          <a
+            href='https://scramblelock.github.io/coding_challenge_11/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={huddle} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Huddle</h3>
+              <p>HTML + CSS + Bootstrap</p>
+            </div>
+          </a>
+        </div>
+        <div className='projectBox'>
+          <a
+            href='https://scramblelock.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <div className='contentOverlay' />
+            <img className='contentImage' src={locking4life} alt='' />
+            <div className='contentDetails fadeInBottom'>
+              <h3>Scramblelock</h3>
+              <p>Wordpress + HTML + CSS</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
